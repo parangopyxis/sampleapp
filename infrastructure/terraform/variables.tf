78dda5b7e8fef378842dda33c82381f1442ac582
Pablo Arango <pablo.arango@pyxisportal.co>
2017-04-19 14:32:34 +0000

    Provisioning Infrastructure using Ansible and not Terraform

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -1,9 +1,9 @@
 variable "aws_access_key" {
-	default = "AKIAIN3SNTS2XMIERR6Q"
+	default = ""
 }
 
 variable "aws_secret_key" {
-	default = "Ai187ESzFXPxWbMw/6hIhKyXbDbcoVqHZ06B/twg"
+	default = ""
 }
 
 variable "aws_region" {
